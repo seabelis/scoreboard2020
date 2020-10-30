11dd85a8aebb6449bd9162b8d3c868c924ae8e84
seabelis <seabelis74@gmail.com>
2020-10-30 17:09:37 +0100

    add sort by score

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -4,16 +4,39 @@ import React, { Component } from "react";
 import Player from "./Player";
 
 export default class Scoreboard extends Component {
-  render() {
+  state = {
+    players: [
+      { id: 1, name: "Kelley", score: 11 },
+      { id: 2, name: "David", score: 14 },
+      { id: 3, name: "Rein", score: 4 }
+    ]
+};
+
+render() {
+  // copying the array of players because `.sort()` **mutates!**
+  const players_copy = [ ...this.state.players ];
+  // sorting the players
+  players_copy.sort((a, b) => b.score - a.score);
+  console.log(players_copy);
+
+  return (
+    <div className="scoreboard">
+      <h1>Scoreboard</h1>
+      <ul>
+        {players_copy.map(this.renderPlayer)}
+      </ul>
+    </div>
+  );
+  }
+
+  renderPlayer = player => {
     return (
-      <div className="scoreboard">
-        <h1>Scoreboard</h1>
-        <ul>
-          <Player name = "Kelly" />
-          <Player name = "David" />
-          <Player name = "Rein" />
-        </ul>
-      </div>
+      <Player
+      id={player.id}
+      name={player.name}
+      score={player.score}
+      key={player.id}
+      />
     )
   }
 }
