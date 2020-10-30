ca977c7351d6d0f9710f9317896e485583aeae8e
seabelis <seabelis74@gmail.com>
2020-10-30 16:45:16 +0100

    add player component

---
+++ src/components/Player.js
@@ -0,0 +1,18 @@
+// components/Player.js
+
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export default class Player extends Component {
+  static propTypes = {
+    name: PropTypes.string.isRequired
+  };
+
+  render() {
+    return (
+      <li className = "player">
+        <p className = "name">{this.props.name</p>
+      </li>
+    )
+  }
+}
