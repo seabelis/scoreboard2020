4e1da9d70dda4feee3a9df23e05cd7d4c2203c68
seabelis <seabelis74@gmail.com>
2020-10-30 19:40:00 +0100

    Add handleChange

--- src/components/AddPlayer.js
+++ src/components/AddPlayer.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
 export default class  AddPlayer extends Component {
-  handleSubmit =(event) =>{
+  state = {name:''}
+
+  handleSubmit = (event) =>{
     event.preventDefault()
-    console.log('submitting form...')
+    console.log(`Submitting form with name ${this.state.name}`)
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
   }
 
   render () {
@@ -12,7 +20,11 @@ export default class  AddPlayer extends Component {
         <form onSubmit={this.handleSubmit}>
           <label>
             Name:
-            <input type="text" name="name" />
+            <input type="text" 
+            name="name"
+            onChange={this.handleChange} 
+            value={this.state.name}
+            />
           </label>
           <input type="submit" value="Add" />
         </form>
